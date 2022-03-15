62a9a4a996dc308ba3ad39a91bc3803dff49537d
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-03-14 22:18:38 -0300

    feat: adicionado funcao de obterValores

--- collections-01/src/main/java/br/fai/ex01/Main.java
+++ collections-01/src/main/java/br/fai/ex01/Main.java
@@ -1,5 +1,9 @@
 package br.fai.ex01;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,6 +11,32 @@ public class Main {
 	}
 
 	private void start() {
+		int i = 0;
+		List<String> valoresDigitados = new ArrayList<String>();
 		
+		while (i < 5) {
+			String valorDigitado = "";
+
+			System.out.print("Digite algum valor: ");
+			valorDigitado = obterDados();
+			
+			if (!valorDigitado.trim().isEmpty() && !valorDigitado.equals("INVALIDO")) {
+				valoresDigitados.add(valorDigitado);
+				i++;
+			}
+		}
+		
+	}
+	
+	private String obterDados() {
+		Scanner scanner = new Scanner(System.in);
+		String valor;
+		try {
+			valor = scanner.nextLine();
+		} catch (Exception e) {
+			valor = "INVALIDO";
+		}
+		
+		return valor;
 	}
 }
