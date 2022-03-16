cab01a9b2f77e5daf1dc829e915e758e9f717498
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-15 22:31:40 -0300

    feat: adicionado metodos da classe gato

--- collections-03/src/main/java/br/fai/ex03/models/Gato.java
+++ collections-03/src/main/java/br/fai/ex03/models/Gato.java
@@ -1,5 +1,22 @@
 package br.fai.ex03.models;
 
 public class Gato extends Animal {
-
+	public Gato () {
+		super();
+		this.setCor("branco");
+	}
+	
+	public Gato (String cor, int idade, String nome) {
+		super(cor, idade, nome);
+	}
+	
+	@Override
+	public void som() {
+		super.som();
+		System.out.println("Eu gosto de miar.");
+	}
+	
+	public void oQueFacoDuranteANoite() {
+		System.out.println("Gosto de pular a cerca toda noite");
+	}
 }
