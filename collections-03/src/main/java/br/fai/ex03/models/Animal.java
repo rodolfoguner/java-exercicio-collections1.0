c042723ad59dd23ec726e4e99f8e00b86b9534b3
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-15 22:17:10 -0300

    feat: adicionado metodos de animal

--- collections-03/src/main/java/br/fai/ex03/models/Animal.java
+++ collections-03/src/main/java/br/fai/ex03/models/Animal.java
@@ -37,5 +37,15 @@ abstract class Animal {
 		this.nome = nome;
 	}
 	
+	public void som() {
+		System.out.println("O som padrão desse animal é: zzzz");
+	}
 	
+	public void dizerNome() {
+		System.out.println("Nome do animal: " + this.getNome());
+	}
+	
+	public void dizerMinhaIdade() {
+		System.out.println("Minha idade é: " + this.getIdade());
+	}
 }
