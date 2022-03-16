d33c12758e478f822c2d15170b6ccadca40c4d52
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-15 22:07:10 -0300

    feat: adicionado atributos da classe animal

--- collections-03/src/main/java/br/fai/ex03/models/Animal.java
+++ collections-03/src/main/java/br/fai/ex03/models/Animal.java
@@ -1,5 +1,41 @@
 package br.fai.ex03.models;
 
 abstract class Animal {
+	private String cor;
+	private int idade;
+	private String nome;
+	
+	public Animal (String cor, int idade, String nome) {
+		this.cor = cor;
+		this.idade = idade;
+		this.nome = nome;
+	}
+	
+	public Animal() {
+		this.cor = "preto";
+		this.idade = 5;
+		this.nome = "não definido";
+	}
 
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
