6173d98d1fd71f45b6eb101345a4349edef39190
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-15 21:25:24 -0300

    feat: adicionado metodo exibe mapa

--- collections-02/src/main/java/br/fai/ex02/Main.java
+++ collections-02/src/main/java/br/fai/ex02/Main.java
@@ -1,5 +1,8 @@
 package br.fai.ex02;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,5 +10,25 @@ public class Main {
 	}
 
 	private void start() {
+		Map<String, String> cidades = new HashMap<String, String>();
+		cidades.put("SRS", "Santa Rita do Sapucai");
+		cidades.put("PA", "Pouso Alegre");
+		cidades.put("ITA", "Itajuba");
+		cidades.put("ZORO", "Conceicao dos Ouros");
+		cidades.put("CAXU", "Cachoeira de Minas");
+		
+		exibeMapa(cidades);
+	}
+	
+	private void exibeMapa(Map<String, String> cidades) {
+		for (String keys: cidades.keySet()) {
+			try {
+			System.out.println("Key: " + keys + ", Valor: " + cidades.get(keys));
+			} catch (Exception e) {
+				System.out.println("Chave " + keys + " não existe");
+			} finally {
+				System.out.println("");
+			}
+		}		
 	}
 }
